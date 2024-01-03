4f0a99c0b342fb1f5ed0ff460692bfc540c5b92f
Evgeniy <evalrez@mail.ru>
2024-01-03 19:53:24 +0300

    add dz3 k seminaru2

---
+++ dz_3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 3: Напишите программу, которая принимает
+// на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+// 40 => 4
+// 96 => 9 40 => 4
+// 72 => 7
+int num = new Random().Next(10, 100);
+Console.WriteLine(num);
+int num1 = num / 10; 
+int num2 = num %10;   
+//Console.WriteLine(num1);
+//Console.WriteLine(num2);
+if (num1 > num2)
+{
+    Console.WriteLine($"{num} => {num1}");
+}
+else
+{
+    Console.WriteLine($"{num} => {num2}");
+}

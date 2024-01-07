48dc4e21d221f1c6e7c468e7f0a83e8770d34ed5
Evgeniy <evalrez@mail.ru>
2024-01-07 22:56:43 +0300

    add dz4 k seminaru2

---
+++ dz_4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 4: Напишите программу, которая на вход
+// принимает натуральное число N, а на выходе
+// показывает его цифры через запятую.
+
+Console.WriteLine("Введите число: ");
+
+string num = Console.ReadLine();
+char s2 = ',';
+
+for (int i = 0; i < num.Length; i++) {
+
+
+    
+    Console.Write($"{num[i]} ");
+    
+    if (i < num.Length-1){
+ 
+    Console.Write(Convert.ToString(s2));
+    }
+}
+
